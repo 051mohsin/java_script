8f9f7366fe5a55e971bc260e9e5e9bb7dd106ac7
051mohsin <mohsinriaz051@gmail>
2024-07-08 21:38:16 +0500

    berifly explane

---
+++ control_flow/switch.js
@@ -0,0 +1,19 @@
+const year = "march";
+
+switch (year) {
+    case "jan":
+        console.log("jan");
+        break;
+
+        case "feb":
+        console.log("feb");
+        break;
+
+        case "march":
+        console.log("march");
+        break;
+
+    default:
+        console.log("this is not year")
+        break;
+}
