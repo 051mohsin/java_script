68838d8a921fb5aeb8b533c242c31afac815b727
051mohsin <mohsinriaz051@gmail>
2024-07-08 22:04:59 +0500

    nullish coalescing operator

--- control_flow/truly_falsly.js
+++ control_flow/truly_falsly.js
@@ -8,3 +8,22 @@ if (loggedInemail && loggedIngoogle , loggedInemail || loggedIngoogle ) {
 else{
     console.log("please tey again");
 }
+
+
+
+
+// nullish coalescing operator
+
+let val1 ;
+
+// val1 = 10 ?? 15 ;
+
+// val1 = null ?? 20;
+
+// val1 = undefined ?? 15
+
+val1 = null ?? 10 ?? 5
+
+
+
+console.log(val1)
