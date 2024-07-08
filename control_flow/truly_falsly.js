783e394cdc147e798d270c6918d8c6242af8eec1
051mohsin <mohsinriaz051@gmail>
2024-07-08 21:44:14 +0500

    truly and falsly

---
+++ control_flow/truly_falsly.js
@@ -0,0 +1,10 @@
+const loggedInemail = []
+const loggedIngoogle = ""
+ 
+if (loggedInemail && loggedIngoogle , loggedInemail || loggedIngoogle ) {
+    console.log("login successfull");
+    
+}
+else{
+    console.log("please tey again");
+}
