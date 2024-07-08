756b918338c8b01be62af733137fefa8304f2527
051mohsin <mohsinriaz051@gmail>
2024-07-08 21:48:09 +0500

    space remove

--- control_flow/truly_falsly.js
+++ control_flow/truly_falsly.js
@@ -3,7 +3,7 @@ const loggedIngoogle = ""
  
 if (loggedInemail && loggedIngoogle , loggedInemail || loggedIngoogle ) {
     console.log("login successfull");
-    
+
 }
 else{
     console.log("please tey again");
