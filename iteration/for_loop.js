7bfe8c1e0103e09d54c0ed5bbfe6359324d3fe96
051mohsin <mohsinriaz051@gmail.com>
2024-12-21 22:53:12 +0500

    iterantion update

--- iteration/for_loop.js
+++ iteration/for_loop.js
@@ -1,12 +1,24 @@
 // foo loop
 
-for (let i = 0; i <= 10 ; i++) {
-    const element = i;
 
-    if (element === 5) {
-        console.log("5 is best number");
+
+
+ const fun = ()=>{
+    for (let i = 1; i <= 10 ; i++) {
+        const element = i;
+    
+        if (element === 11) {
+            console.log("is best number");
+            break;
+            
+        }
+        else{
+            console.log("not equal to")
+        }
         
     }
-    console.log(element)
-    
-}
+
+
+
+ }
+ fun()
